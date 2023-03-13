f6b283fb4a93f559b590544a89f7e9e84c60cc25
Fofan15 <daniil.grishin16@gmail.com>
2023-03-13 20:15:04 +0200

    Add a code

---
+++ CodeHomeWork_3_2/CodeHomeWork_3_2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeHomeWork_3_2
+{
+    public class Class1
+    {
+        public delegate void ShowDelegate (bool value);
+
+        public  double Pow(double x, double y) 
+        {
+            double result = x * y;
+
+            return result;
+        }
+    }
+}
